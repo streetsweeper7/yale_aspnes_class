7ba11cba672bd57c271862cf16851f93eb5bb107
jbanda <adrielbanda4@gmail.com>
2024-04-07 21:38:27 +0100

    Added return value definitions

--- chapter_4_1/sumRange.c
+++ chapter_4_1/sumRange.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// define some return constants
+#define SUCCESS 0
+#define ERROR_INSUFFICENT_ARGUMENTS 1
+
 int sumRange(int begin, int end)
 {
     int sum = 0;        // Sum of the integers between begin and end
@@ -19,12 +23,16 @@ int main (int argc, char **argv)
     {
         // argv[0] is the name of the program. Because the arguments include 
         // the name of the program
-        fprintf(stderr, "Usage %s begin end", argv[0]);
+        fprintf(stderr, "Usage %s begin end\n", argv[0]);
+
+        return ERROR_INSUFFICENT_ARGUMENTS;
     }
     int begin = atoi(argv[1]); // Convert the second argument, begin, to an integer from string
     int end = atoi(argv[2]); // Convert the third argument,end, to an integer from a string
 
     printf("sumRange(%d, %d)=%d\n", begin, end, sumRange(begin, end));
+
+    return SUCCESS;
 
     return 0;
 }
